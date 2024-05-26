ac5f1353e0867cefe48f6e8bdf6084c45cb9c32b
Doesn't Code <160818162+AndoJoshuaBSIT32E1@users.noreply.github.com>
2024-05-26 21:04:49 +0800

    Add files via upload
    uploaded the files

---
+++ BSIT32E1_Student_Assessment/BSIT32E1_Student_Assessment/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BSIT32E1_Student_Assessment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BSIT32E1_Student_Assessment/BSIT32E1_Student_Assessment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BSIT32E1_Student_Assessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
